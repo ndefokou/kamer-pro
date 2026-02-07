511ea8cc574451a8b707d3f6440dfdcd7b8c388b
ndefokou <tcheutchouaarthur38@gmail.com>
2026-02-07 16:38:43 +0100

    Deploying the app on github pages

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,6 +7,7 @@ import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/kamer-pro/",
   server: {
     host: "::",
     port: 8080,
