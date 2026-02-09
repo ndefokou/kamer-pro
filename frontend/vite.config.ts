0e35feaca63b39b61f57d693e96b2476724fe213
ndefokou <tcheutchouaarthur38@gmail.com>
2026-02-09 16:13:37 +0100

    fix deployment file

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ import compression from 'vite-plugin-compression';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/",
+  base: "./",
   server: {
     host: "::",
     port: 8080,
